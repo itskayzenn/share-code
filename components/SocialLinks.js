e3e54e04cc4677ca6625604d44a7bd331ca154ca
K ɪɴꜰɪɴɪᴛʏ <amnakhoirulzamzam10@gmail.com>
2026-01-10 09:19:57 +0700

    Create SocialLinks.js

---
+++ components/SocialLinks.js
@@ -0,0 +1,11 @@
+export default function SocialLinks() {
+  return (
+    <footer>
+      <a href="https://github.com/username">GitHub</a>
+      <a href="https://wa.me/628xxxx">WhatsApp</a>
+      <a href="https://instagram.com/username">Instagram</a>
+      <a href="https://t.me/username">Telegram</a>
+      <a href="https://whatsapp.com/channel/xxxx">WA Channel</a>
+    </footer>
+  )
+}
