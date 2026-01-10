23bf6a8824fb9544daac23f06015cd24534550fe
K ɪɴꜰɪɴɪᴛʏ <amnakhoirulzamzam10@gmail.com>
2026-01-10 09:29:08 +0700

    Delete components directory

--- components/CodeCard.js
+++
@@ -1,9 +0,0 @@
-export default function CodeCard({ title, language, code }) {
-  return (
-    <div className="card">
-      <h3>{title}</h3>
-      <small>{language}</small>
-      <pre>{code}</pre>
-    </div>
-  )
-}
